5451bb99f9ed74344640e5c11a3e6b71d23fc57b
Mehmet Olgun <mehmet.olgun@hepsiburada.com>
2022-08-24 14:06:47 +0300

    project initialized

---
+++ BackgroundWorker-Hangfire/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using Hangfire;
+
+namespace BackgroundWorker_Hangfire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            try
+            {
+                GlobalConfiguration.Configuration.UseSqlServerStorage(
+                    "Data Source=localhost,1433;Initial Catalog=BackgroundWorker;User Id=sa;Password=Password123;");
+                RecurringJob.AddOrUpdate("clock",()=>WriteTime(),Cron.Minutely);
+               
+                using (new BackgroundJobServer())
+                {
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        
+
+        }
+
+        public static void WriteTime()
+        {
+            string d = DateTime.Now.ToString("HH:mm:ss");
+            Console.WriteLine(d);
+
+        }
+    }
+}
